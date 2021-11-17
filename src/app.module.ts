b3b1a4e6e56fc9f0eba52ab4b8f8fdeecaae914f
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-11-17 17:20:09 +0100

    Move to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,28 @@ import { FramesController } from './controllers/frames.controller';
 import { FramesService } from './services/frames.service';
 import { AuthService } from './services/auth.service';
 import { HttpStrategy } from './config/http.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'ramka.sqlite',
-      entities: [Frame, Photo],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        ssl:
+          configService.get('DB_SSL') === 'true'
+            ? { rejectUnauthorized: false }
+            : false,
+        entities: [Frame, Photo],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Frame, Photo]),
     MulterModule.register({
