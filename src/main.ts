51f1e3b27beec097a9f41df296cf1b3c572ba0d4
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-11-17 17:31:25 +0100

    Update port

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
